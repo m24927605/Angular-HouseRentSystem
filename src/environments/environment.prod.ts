24d1d4697920f71c0015233477e94f7bee7f97b1
Michael <m24927605@gmail.com>
2018-02-03 22:22:14 +0800

    修改port

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // 上線時請再改成正試機
   RestApiIP: 'https://rentsystem-server.herokuapp.com',
-  RestApiRoute:'/api'
+  RestApiRoute:'http://54.238.245.231:3000/api'
 };
