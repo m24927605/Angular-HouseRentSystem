abddddfd77b5194f3096e80d21e3eafccd89cb5b
Michael <m24927605@gmail.com>
2018-02-03 23:12:42 +0800

    新增dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // 上線時請再改成正試機
-  RestApiIP: 'http://54.238.245.231:3000',
-  RestApiRoute:'http://54.238.245.231:3000/api'
+  RestApiIP: 'https://rentsystem-server.herokuapp.com',
+  RestApiRoute:'/api'
 };
